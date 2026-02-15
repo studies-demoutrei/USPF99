edb022b96712ed58f78d72560d4d2bac3f991b9a
studies-demoutrei <studies.demoutrei@gmail.com>
2026-02-15 23:25:50 +0800

    Added header

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const headerIconElement = document.querySelector("#header-icon");
+const headerTitleElement = document.querySelector("#header-title");
+
+
+headerIconElement.addEventListener("click", (_) => window.open("https://uspf.edu.ph", "_blank"));
+headerTitleElement.addEventListener("click", (_) => window.open("https://uspf.edu.ph", "_blank"));
