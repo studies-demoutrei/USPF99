8ec5413158c0636c3cd46830c75152ef8f394dbb
studies-demoutrei <studies.demoutrei@gmail.com>
2026-02-17 20:16:05 +0800

    Added banner

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,5 @@ const headerIconElement = document.querySelector("#header-icon");
 const headerTitleElement = document.querySelector("#header-title");
 
 
-headerIconElement.addEventListener("click", (_) => window.open("https://uspf.edu.ph", "_blank"));
-headerTitleElement.addEventListener("click", (_) => window.open("https://uspf.edu.ph", "_blank"));
+headerIconElement.addEventListener("click", (_) => window.location.href = "https://uspf.edu.ph");
+headerTitleElement.addEventListener("click", (_) => window.location.href = "https://uspf.edu.ph");
